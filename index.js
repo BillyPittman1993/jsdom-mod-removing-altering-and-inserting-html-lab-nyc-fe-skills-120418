d8ae3e077d6863de33c85b7ee154827b72d0f799
BillyPittman1993 <billytrading93@gmail.com>
2019-02-08 01:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
 
+main.remove()
+
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+document.body.appendChild('id')
+
+
+ 
